ba53207f9d8799fdca3fbdabd5e55640f9f5a593
contactus-ops <contactus@wearthactive.com>
2026-04-24 18:05:06 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, send_from_directory, send_file
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return send_file('index.html')
+
+@app.route('/manifest.json')
+def manifest():
+    return send_file('manifest.json')
+
+@app.route('/sw.js')
+def sw():
+    return send_file('sw.js')
+
+@app.route('/<path:path>')
+def static_files(path):
+    return send_from_directory('.', path)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
